35b812655d57d12b4f930426fe795488e2b5b281
William <william.zerbato@gmail.com>
2019-05-18 11:58:52 -0400

    Chrono + Targets list

---
+++ GameJam-Pot_au_jeu_2/Assets/Script/chronoManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class chronoManager : MonoBehaviour
+{
+    public Slider slider;
+
+    [SerializeField]
+    private int timeDivider;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        slider.value += Time.deltaTime/timeDivider;
+    }
+}
