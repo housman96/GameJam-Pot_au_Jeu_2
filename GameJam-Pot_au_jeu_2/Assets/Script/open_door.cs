40a2e3b1bb23a1f8f814c9b050501f0a703f1d02
Zeppo <theo-henry@orange.fr>
2019-05-18 02:08:06 -0400

    asset porte 1 + fond timer

---
+++ GameJam-Pot_au_jeu_2/Assets/Script/open_door.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class open_door : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Collider m_Collider;
+    private Renderer m_Renderer;
+
+    void Start()
+    {
+        m_Collider = GetComponent<Collider>();
+        m_Collider.enabled = true;
+        m_Renderer = GetComponent<Renderer>();
+        m_Renderer.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            //Toggle the Collider on and off when pressing the space bar
+            m_Collider.enabled = false;
+            m_Renderer.enabled = false;
+
+            //Output to console whether the Collider is on or not
+            Debug.Log("Collider.enabled = " + m_Collider.enabled);
+            Debug.Log("Renderer.enabled = " + m_Renderer.enabled);
+        }
+
+    }
+}
