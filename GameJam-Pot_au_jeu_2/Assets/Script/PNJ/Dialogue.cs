81b58afd8fc9286b1adb8d500a036f8d61115c4c
Griwald <griwald13@gmail.com>
2019-05-18 16:11:34 -0400

    Systeme de villageois de maison et d'indices

---
+++ GameJam-Pot_au_jeu_2/Assets/Script/PNJ/Dialogue.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dialogue : MonoBehaviour
+{
+	GameObject[] aliveVillagers;
+
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		aliveVillagers = GameObject.FindGameObjectsWithTag("Villager");
+		Shuffle(aliveVillagers);
+
+		int numVillagers = aliveVillagers.Length;
+		for (int i = 0; i < numVillagers; i++) {
+			Villager current = aliveVillagers[i].GetComponent<Villager>();
+			current.villagerToTalk1 = aliveVillagers[(i + 1) % numVillagers].GetComponent<Villager>();
+			current.villagerToTalk2 = aliveVillagers[(i + 2) % numVillagers].GetComponent<Villager>();
+			current.villagerToTalk3 = aliveVillagers[(i + 3) % numVillagers].GetComponent<Villager>();
+		}
+
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			foreach (GameObject villager in aliveVillagers) {
+				Debug.Log("=================");
+				villager.GetComponent<Villager>().Print();
+			}
+		}
+	}
+
+	void Shuffle(GameObject[] array)
+	{
+		for (int t = 0; t < array.Length; t++) {
+			GameObject tmp = array[t];
+			int r = Random.Range(t, array.Length);
+			array[t] = array[r];
+			array[r] = tmp;
+		}
+	}
+
+}
---
+++ GameJam-Pot_au_jeu_2/Assets/Script/PNJ/NPCInitializer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCInitializer : MonoBehaviour
+{
+	public List<GameObject> villagers;
+	public List<GameObject> houses;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (GameObject villager in villagers) {
+			int randomIndex = Random.Range(0, houses.Count);
+			GameObject randHouse = houses[randomIndex];
+			houses.RemoveAt(randomIndex);
+
+			villager.GetComponent<Villager>().house = randHouse.GetComponent<House>();
+		}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		//if (Input.GetKeyDown(KeyCode.Space)) {
+		//	foreach (GameObject villager in villagers) {
+		//		Debug.Log("=================");
+		//		villager.GetComponent<Villager>().Print();
+		//	}
+		//}
+    }
+}
