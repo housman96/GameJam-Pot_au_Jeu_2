98d3432f6ba610237e03cc3392f9d67bd595de03
Julie <juliemounier96@gmail.com>
2019-05-19 16:22:44 -0400

    GameOver fini, manque musique

---
+++ GameJam-Pot_au_jeu_2/Assets/Script/Menu/GameoverHandler.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameoverHandler : MonoBehaviour
+{
+    public GameObject txtJ1;
+    public GameObject txtJ2;
+
+    // TODO : change bool
+    private bool isJ1Winner = true;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (isJ1Winner)
+        {
+            txtJ1.SetActive(true);
+            txtJ2.SetActive(false);
+        }
+        else
+        {
+            txtJ1.SetActive(false);
+            txtJ2.SetActive(true);
+        }
+    }
+
+    public void onClickNewGame()
+    {
+        SceneManager.LoadScene(0);
+        Debug.Log("Restart!");
+    }
+
+    public void onClickExit()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+}
