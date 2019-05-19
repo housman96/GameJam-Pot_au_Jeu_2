4847fca684567c5cd9c266b71b0c5b5d71736eab
Ombeline Leclerc-Istria <o.leclerc.istria@gmail.com>
2019-05-19 09:22:52 -0400

    Trigger perso communication + scene bubbles

---
+++ GameJam-Pot_au_jeu_2/Assets/TalkOnCollider.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TalkOnCollider : MonoBehaviour
+{
+    public GameObject PanelP1;
+    public GameObject PanelP2;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        Debug.Log("Coucou");
+        if (Input.GetKey(KeyCode.E))
+        {
+            Debug.Log("Coucou");
+
+            Villager current = gameObject.GetComponent<Villager>();
+
+            string Nom1 = current.villagerToTalk1.villagerName;
+            string Nom2 = current.villagerToTalk2.villagerName;
+            string Nom3 = current.villagerToTalk3.villagerName;
+
+            string Taille;
+            if (current.villagerToTalk1.house.size == Size.Large) { Taille = "large"; }
+            else if (current.villagerToTalk1.house.size == Size.Medium) { Taille = "medium"; }
+            else { Taille = "small"; }
+
+            string Porte;
+            if (current.villagerToTalk1.house.door == Door.Brown) { Porte = "brown"; }
+            else if (current.villagerToTalk1.house.door == Door.Blue) { Porte = "blue"; }
+            else { Porte = "green"; }
+
+            string Objet;
+            if (current.villagerToTalk1.house.deco == Deco.Barrel) { Objet = "barrel"; }
+            else if (current.villagerToTalk1.house.deco == Deco.Seat) { Objet = "seat"; }
+            else { Objet = "ladder"; }
+
+            PanelP1.GetComponent<ShowDialog>().SetText(Nom1, Nom2, Nom3, Taille, Porte, Objet);
+            PanelP1.SetActive(true);
+            
+        }
+    }
+}
