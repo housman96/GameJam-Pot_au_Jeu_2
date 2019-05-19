48c2629e58e197cf480b8a09e2f579e44ece7d24
Julie <juliemounier96@gmail.com>
2019-05-19 15:24:16 -0400

    Menu principal fini, manque musique

--- GameJam-Pot_au_jeu_2/Assets/Script/Menu/MenuHandler.cs
+++ GameJam-Pot_au_jeu_2/Assets/Script/Menu/MenuHandler.cs
@@ -4,17 +4,26 @@ using UnityEngine.SceneManagement;
 
 public class MenuHandler : MonoBehaviour
 {
-    public GameObject canvasMenu;
-    public GameObject canvasCredits;
+    public GameObject panelMenu;
+    public GameObject panelTutoriel;
+    public GameObject panelCredits;
 
     public void Start()
     {
-        canvasCredits.SetActive(false);
+        panelCredits.SetActive(false);
+        panelTutoriel.SetActive(false);
+        panelMenu.SetActive(true);
     }
 
     public void onClickNewGame()
     {
         SceneManager.LoadScene(1);
+        Debug.Log("Play!");
+    }
+
+    public void onClickTutorial()
+    {
+        StartCoroutine("menuToTutorial");
     }
 
     public void onClickCredits()
@@ -22,54 +31,115 @@ public class MenuHandler : MonoBehaviour
         StartCoroutine("menuToCredits");
     }
 
-    public void onClickMenu()
+    public void onClickMenu(bool isCredit)
     {
-        StartCoroutine("creditsToMenu");
+        if (isCredit)
+            StartCoroutine("creditsToMenu");
+        else
+            StartCoroutine("tutorielToMenu");
     }
+
+    public void onClickExit()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    } 
 
     public IEnumerator menuToCredits()
     {
-        float a = 1;
-        while (a >= 0)
-        {
-            yield return new WaitForSeconds(0.01f);
-            setAlphaObject(canvasMenu, a);
-            a -= 0.05f;
-        }
-        canvasMenu.SetActive(false);
+        float a = 0;
 
-        yield return new WaitForSeconds(0.5f);
-        canvasCredits.SetActive(true);
-        setAlphaObject(canvasCredits, 0);
+        setAlphaObject(panelCredits, 0);
+        panelCredits.SetActive(true);
+
         while (a <= 1)
         {
             yield return new WaitForSeconds(0.01f);
-            setAlphaObject(canvasCredits, a);
+            setAlphaObject(panelCredits, a);
             a += 0.05f;
         }
+        yield return new WaitForSeconds(0.5f);
 
+        a = 1;
+        while (a >= 0)
+        {
+            yield return new WaitForSeconds(0.01f);
+            setAlphaObject(panelMenu, a);
+            a -= 0.05f;
+        }
+        panelMenu.SetActive(false);
+    }
+
+    public IEnumerator menuToTutorial()
+    {
+        float a = 0;
+
+        setAlphaObject(panelTutoriel, 0);
+        panelTutoriel.SetActive(true);
+
+        while (a <= 1)
+        {
+            yield return new WaitForSeconds(0.01f);
+            setAlphaObject(panelTutoriel, a);
+            a += 0.05f;
+        }
+        yield return new WaitForSeconds(0.5f);
+
+        a = 1;
+        while (a >= 0)
+        {
+            yield return new WaitForSeconds(0.01f);
+            setAlphaObject(panelMenu, a);
+            a -= 0.05f;
+        }
+        panelMenu.SetActive(false);
     }
 
     public IEnumerator creditsToMenu()
     {
         float a = 1;
-        while (a >= 0)
-        {
-            yield return new WaitForSeconds(0.01f);
-            setAlphaObject(canvasCredits, a);
-            a -= 0.05f;
-        }
-        canvasCredits.SetActive(false);
-
-        yield return new WaitForSeconds(0.5f);
-        canvasMenu.SetActive(true);
-        setAlphaObject(canvasMenu, 0);
+        setAlphaObject(panelMenu, 0);
+        panelMenu.SetActive(true);
         while (a <= 1)
         {
             yield return new WaitForSeconds(0.01f);
-            setAlphaObject(canvasMenu, a);
+            setAlphaObject(panelMenu, a);
             a += 0.05f;
         }
+        yield return new WaitForSeconds(0.5f);
+
+        a = 1;
+        while (a >= 0)
+        {
+            yield return new WaitForSeconds(0.01f);
+            setAlphaObject(panelCredits, a);
+            a -= 0.05f;
+        }
+        panelCredits.SetActive(false);
+
+    }
+
+    public IEnumerator tutorielToMenu()
+    {
+        float a = 1;
+        setAlphaObject(panelMenu, 0);
+        panelMenu.SetActive(true);
+        while (a <= 1)
+        {
+            yield return new WaitForSeconds(0.01f);
+            setAlphaObject(panelMenu, a);
+            a += 0.05f;
+        }
+        yield return new WaitForSeconds(0.5f);
+
+        a = 1;
+        while (a >= 0)
+        {
+            yield return new WaitForSeconds(0.01f);
+            setAlphaObject(panelTutoriel, a);
+            a -= 0.05f;
+        }
+        panelTutoriel.SetActive(false);
 
     }
 
