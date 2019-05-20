e9580a4c1aeb8af4613f637dd96f96ca384c3b6d
Julie <juliemounier96@gmail.com>
2019-05-19 21:00:20 -0400

    Intro faite, manque bruits

---
+++ GameJam-Pot_au_jeu_2/Assets/Script/Menu/DialogueTrigger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public Dialoguee dialogue;
+
+    public void TriggerDialogue()
+    {
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+        Debug.Log("Bleh");
+    }
+}
